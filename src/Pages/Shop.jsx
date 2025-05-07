125c207099289033fd37116b91d0e52ea340dc7f
Alisha Gupta <alishagupta2004gupta@gmail.com>
2025-05-07 16:58:11 +0530

    ff

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/popular/Popular'
+import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from "../Components/NewCollections/NewCollections"
-import Newsletter from '../Components/NewsLetter/Newsletter'
+import Newsletter from '../Components/NewsLetter/NewsLetter'
 import Footer from '../Components/Footer/Footer'
 
 
